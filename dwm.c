72535ea29b1068191c38690dcebcece68c362798
Basilios <vasyl.tyshchuk.xyz@gmail.com>
2023-05-02 20:33:57 +0300

    Replace rectangle with underline to indicate active tags
    Based on these patches:
    - https://dwm.suckless.org/patches/underlinetags
    - https://dwm.suckless.org/patches/activetagindicatorbar

--- dwm.c
+++ dwm.c
@@ -934,9 +934,9 @@ drawbar(Monitor *m)
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
 		if (occ & 1 << i)
-			drw_rect(drw, x + boxs, boxs, boxw, boxw,
-				m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
-				urg & 1 << i);
+			drw_rect(drw, x + ulinepad, bh - ulinestroke - ulinevoffset, w - (ulinepad * 2), ulinestroke,
+			 	m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
+			 	urg & 1 << i);
 		x += w;
 	}
 	w = TEXTW(m->ltsymbol);
